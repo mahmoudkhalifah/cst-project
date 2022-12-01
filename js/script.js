df06324f89f9c9501a4d2c4c7b3b22f08da7e16d
Noura Mahmoud <nouramahmoud099@gmail.com>
2022-12-01 17:41:49 +0200

    modify categories

--- js/script.js
+++ js/script.js
@@ -1,68 +1,9 @@
-
 function init() {
     document.getElementById("addTaskBtn").onclick = addTask;
     //set default date as today's date
     document.getElementById("dateInput").valueAsDate = new Date();
-}
 
-let categories = [];
-// let categories = [{"newTaskCategory":"school", "color":"red"}];
-
-
-document.getElementById("category").style.display = "none";
-
-submitNewCategory = document.getElementById("submitNewCategoryBtn");
-submitNewCategory.addEventListener("click",addNewCategory);
-togCategoryBtn = document.getElementById("popUpBtn");
-togCategoryBtn.addEventListener("click", toggleCategoryBtn);
-
-function toggleCategoryBtn()
-{
-    if (togCategoryBtn.value == 'Add new category')
-    {
-        togCategoryBtn.value = 'close';
-        showNewCategoryDiv();
-    }
-    else if (togCategoryBtn.value == 'close')
-    {
-        togCategoryBtn.value = 'Add new category';
-        hideNewCategoryDiv();
-    }
-}
-
-function showNewCategoryDiv()
-{
-    document.getElementById("category").style.display = "block";
-}
-
-function hideNewCategoryDiv()
-{
     document.getElementById("category").style.display = "none";
-    document.getElementById("newCategoryNameInput").value = "";
-}
-
-function addNewCategory()
-{
-    let cat_value = document.getElementById("newCategoryNameInput").value;
-    let cat_color = document.getElementById("newCategoryColorInput").value;
-    if (cat_value)
-    {
-        categories.push({"newTaskCategory": cat_value, "color": cat_color});
     
-        updateCtegories();
-        console.log(categories);
-    }
-    else
-    {
-        alert("You didn't enter the category name!")
-    }
+    initCategories();
 }
-
-function updateCtegories()
-{
-    let categoriesListSelect = document.getElementById("newTaskCategorySelect");
-    let option = document.createElement("option");
-    option.text = categories[categories.length - 1]["newTaskCategory"];
-    categoriesListSelect.append(option);
-}
-
