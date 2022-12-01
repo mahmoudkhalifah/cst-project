9ea80fec7bb5075c6300f234f8467aa204491b75
Mahmoud khalifa <eng.mahmoudkhalifah@gmail.com>
2022-12-01 16:31:44 +0200

    added status

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ var tasks = [];
     tasks list structure
     [
         "22-12-2022": [
-            ["clean my room","home"],
-            ["make lunch","food"],
-            ["study math","school"]
+            ["clean my room","home","todo"],
+            ["make lunch","food","done"],
+            ["study math","school","todo"]
         ],
         "25-12-2022": [
-            ["wash the dishes","home"],
-            ["make dinner","food"],
+            ["wash the dishes","home","todo"],
+            ["make dinner","food","todo"],
         ]
     ]
 */
@@ -28,17 +28,18 @@ function addTask () {
     var taskCategory = document.getElementsByName("newTaskCategory")[taskCategoryIndex].value;
     if(date && taskName) {
         if(tasks[date]) 
-            tasks[date].push([taskName,taskCategory]);
+            tasks[date].push([taskName,taskCategory,"todo"]);
         else {
             tasks[date] = [];
-            tasks[date].push([taskName,taskCategory]);
+            tasks[date].push([taskName,taskCategory,"todo"]);
         }
         document.getElementById("newTaskNameInput").value="";
         addTaskNode();
     } else {
         alert("choose date and enter task name");
     }
-    console.log(tasks["2022-12-01"]);
+    // console.log(tasks["2022-12-01"]);
+    console.log(tasks);
 }
 
 function addTaskNode() {
