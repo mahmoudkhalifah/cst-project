3dd07d90dd78ce0c0caac3c87f4d5ef645b8c606
Mahmoud khalifa <eng.mahmoudkhalifah@gmail.com>
2022-12-01 17:08:29 +0200

    added seprated script for task

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 
-function init(){}
+function init() {
+    document.getElementById("addTaskBtn").onclick = addTask;
+    //set default date as today's date
+    document.getElementById("dateInput").valueAsDate = new Date();
+}
 
 let categories = [];
 // let categories = [{"newTaskCategory":"school", "color":"red"}];
@@ -60,53 +64,5 @@ function updateCtegories()
     let option = document.createElement("option");
     option.text = categories[categories.length - 1]["newTaskCategory"];
     categoriesListSelect.append(option);
-}
-
-var tasks = [];
-/*
-    tasks list structure
-    [
-        "22-12-2022": [
-            ["clean my room","home","todo"],
-            ["make lunch","food","done"],
-            ["study math","school","todo"]
-        ],
-        "25-12-2022": [
-            ["wash the dishes","home","todo"],
-            ["make dinner","food","todo"],
-        ]
-    ]
-*/
-
-function init() {
-    document.getElementById("addTaskBtn").onclick = addTask;
-    //set default date as today's date
-    document.getElementById("dateInput").valueAsDate = new Date();
-}
-
-
-function addTask () {
-    var date = document.getElementById("dateInput").value;
-    var taskName = document.getElementById("newTaskNameInput").value;
-    var taskCategoryIndex = document.getElementById("newTaskCategorySelect").selectedIndex;
-    var taskCategory = document.getElementsByName("newTaskCategory")[taskCategoryIndex].value;
-    if(date && taskName) {
-        if(tasks[date]) 
-            tasks[date].push([taskName,taskCategory,"todo"]);
-        else {
-            tasks[date] = [];
-            tasks[date].push([taskName,taskCategory,"todo"]);
-        }
-        document.getElementById("newTaskNameInput").value="";
-        addTaskNode();
-    } else {
-        alert("choose date and enter task name");
-    }
-    // console.log(tasks["2022-12-01"]);
-    console.log(tasks);
-}
-
-function addTaskNode() {
-
 }
 
